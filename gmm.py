926512b8c6e14f6d609d86ccbcb5bf543856ba13
Lam Chi Thanh <lamchithanh1997@gmail.com>
2019-04-16 14:32:40 +0800

    Add keras vae + gmm model

---
+++ gmm.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+import numpy as np
+
+# Generate some data
+from sklearn.datasets.samples_generator import make_blobs
+X, y_true = make_blobs(n_samples=400, centers=4,
+                       cluster_std=0.60, random_state=0)
+X = X[:, ::-1] # flip axes for better plotting
+
+from sklearn.mixture.gaussian_mixture import GaussianMixture
+gmm = GaussianMixture(n_components=4).fit(X)
+labels = gmm.predict(X)
+plt.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis')
+plt.show()
